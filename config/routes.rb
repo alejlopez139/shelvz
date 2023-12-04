a0b926d10f4027bc17aee5c99247be201f3283dc
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-12-04 17:27:07 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :books
   resources :bookshelves
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  get "/:username" => "users#show", as: :user
 end
