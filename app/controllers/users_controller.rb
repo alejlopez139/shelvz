dec98f713e19890a991a20d1f758c51f35e19372
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-12-04 22:40:30 +0000

    reorders bookshelves by updated_at, added cards for them as well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(username: params[:username])
+    @bookshelves = @user.bookshelves.order(updated_at: :desc)
   end
 end
